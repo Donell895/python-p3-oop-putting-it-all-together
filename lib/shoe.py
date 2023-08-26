1cb91d80e394c475107333b7d1d577eb0384abb3
Donell <donell.wambua@student.moringaschool.com>
2023-08-26 23:00:58 +0300

    Completed assignment

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+        self.condition = "New"
+
+    def cobble(self):
+        self.condition = "New"
+        return "The shoe has been repaired."
+
+    
